07d40610a3390ac3f299099f062f21889297e3b5
egarciadececa <e.garciadececa@gmail.com>
2022-07-15 10:41:58 +0200

    Configure base styles on Storybook

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,3 +1,9 @@
+import { ThemeProvider } from "styled-components";
+import { Normalize } from "styled-normalize";
+
+import GlobalStyle from "../src/GlobalStyle";
+import { darkTheme, lightTheme } from "../src/themes";
+
 export const parameters = {
   actions: { argTypesRegex: "^on[A-Z].*" },
   controls: {
@@ -6,4 +12,16 @@ export const parameters = {
       date: /Date$/,
     },
   },
-}
+};
+
+const withBackground = (Story, context) => {
+  return (
+    <ThemeProvider theme={darkTheme}>
+      <Normalize />
+      <GlobalStyle />
+      <Story {...context} />
+    </ThemeProvider>
+  );
+};
+
+export const decorators = [withBackground];
