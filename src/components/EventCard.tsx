9006cd58fee5f2410bd9fa14c62ba3b8dbe277db
Eric García de Ceca <eric.garciadececa@clikalia.com>
2022-11-21 10:48:08 +0100

    Color event cards

--- src/components/EventCard.tsx
+++ src/components/EventCard.tsx
@@ -1,7 +1,12 @@
 import React from "react";
 
 import Card from "./base/Card";
-import { Event } from "../services/constants-types";
+import {
+  checkEnumExhausted,
+  Event,
+  EventState,
+} from "../services/constants-types";
+import { Color } from "../themes";
 
 const formatDate = (date: Date) =>
   date.toLocaleDateString("es-ES", {
@@ -18,12 +23,26 @@ interface EventCardProps {
 
 export default function EventCard(props: EventCardProps) {
   const { event } = props;
+
+  function getColor(state: EventState): Color {
+    switch (state) {
+      case EventState.PUBLIC:
+        return "green";
+      case EventState.PRIVATE:
+        return "cyan";
+      case EventState.DRAFT:
+        return "yellow";
+      default:
+        return checkEnumExhausted(state);
+    }
+  }
   return (
     <Card
       title={event.headline}
       center={<p>{event.description || "No description"}</p>}
       leftCorner={<p>{event.state}</p>}
       rightCorner={<p>{formatDate(event.startDate)}</p>}
+      color={getColor(event.state)}
     />
   );
 }
