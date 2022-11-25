e7535a393dbabb571cb7389c975db67373f5e49b
Eric García de Ceca <eric.garciadececa@clikalia.com>
2022-11-25 08:50:38 +0100

    Fix Bubble stories

--- src/components/base/Bubble.stories.tsx
+++ src/components/base/Bubble.stories.tsx
@@ -5,18 +5,27 @@ import Bubble from "./Bubble";
 import { ReactComponent as MessageIcon } from "../../assets/message.svg";
 
 export default {
-  title: "Components/Base/FloatingBubble",
+  title: "Components/Base/Bubble",
   component: Bubble,
+  argTypes: {
+    color: {
+      options: ["neutral", "cyan", "green", "purple", "yellow", "red"],
+      control: { type: "select" },
+    },
+  },
 } as ComponentMeta<typeof Bubble>;
 
-const Template: ComponentStory<typeof Bubble> = (args) => (
-  <Bubble {...args}>
-    <MessageIcon />
-  </Bubble>
-);
+const Template: ComponentStory<typeof Bubble> = (args) => <Bubble {...args} />;
 
 export const Default = Template.bind({});
 Default.args = {
   size: 64,
-  offset: 32,
+  color: "neutral",
+};
+
+export const WithIcon = Template.bind({});
+WithIcon.args = {
+  size: 64,
+  color: "neutral",
+  children: <MessageIcon />,
 };
